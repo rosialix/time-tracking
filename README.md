df0934c3a16247c2d325e9b4297c3b3d2e23095c
rosialix <rosialix@gmail.com>
2021-10-26 13:56:37 -0400

    New

---
+++ README.md
@@ -0,0 +1,59 @@
+﻿# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+
+
+### Screenshot
+
+![](./images/Desktop.png)
+
+
+
+### Links
+
+- Solution URL: [Solution URL](https://github.com/rosialix/Time-tracking-dashboard)
+- Live Site URL: [Live Site URL](https://rosialix.github.io/Time-tracking-dashboard/)
+
+## My process
+For this challenge start the analysis for Css Grid.
+This was my first challenge applying Javascript to develop a page with json reading
+For the manipulation of the json, the content of the Frontend contributor Mentor Mikhail Kulikov was of great help.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Next.js](https://nextjs.org/) - React framework
+
+
+
+## Author
+
+- Frontend Mentor - [@rosialix](https://www.frontendmentor.io/profile/rosialix)
+
+
+
+## Acknowledgments
+
+For the manipulation of the json, the content of the Frontend contributor Mentor Mikhail Kulikov was of great help.
---
+++ app.js
@@ -0,0 +1,80 @@
+//Captura los datos del .json
+async function getData(data_json = 'data.json') {
+    const response = await fetch(data_json);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+class Box_cards {
+    //declara objeto 
+    static TIMES = {
+        daily: 'day',
+        weekly: 'week',
+        monthly: 'month',
+    }
+
+    constructor(data, container = '.ficha__content', view = 'weekly') {
+        this.data = data;
+        this.container = document.querySelector(container);
+        this.view = view;
+
+        this.createMarkup();
+    }
+
+    createMarkup() {
+        const { title, timeframes } = this.data;
+        const { current, previous } = timeframes[this.view.toLowerCase()];
+        const id = title.toLowerCase().replace(/ /g, '-');
+
+        this.container.insertAdjacentHTML("beforeend",
+            `<div class="contenedor__item contenedor__item--${id}">
+        <article class="cards">
+        <header class="cards__header">
+          <h4 class="cards__title">${title}</h4>
+          <img class="cards__menu" src="./images/icon-ellipsis.svg" alt="menu">
+        </header>
+        <div class="cards__body">
+            <div class="cards__time">
+              ${current}hrs
+            </div>
+            <div class="cards__prev-period">
+              Last ${Box_cards.TIMES[this.view]} - ${previous}hrs
+            </div>
+        </div>
+      </article>
+    </div>`);
+    
+        this.time = document.querySelector(`.contenedor__item--${id} .cards__time`);
+        this.prev = document.querySelector(`.contenedor__item--${id} .cards__prev-period`);
+    }
+
+    cambioVista(view) {
+        this.view = view;
+        const { current, previous } = this.data.timeframes[this.view.toLowerCase()];
+
+        this.time.innerText = `${current}hrs`;
+        this.prev.innerText = `Last ${Box_cards.TIMES[this.view]} - ${previous}hrs`;
+
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getData().
+        then(data => {
+            const activities = data.map(activity => new Box_cards(activity));
+            const selectors = document.querySelectorAll('.view-selector__item');
+
+            selectors.forEach(selector => {
+                selector.addEventListener('click', (e) => {
+                    selectors.forEach(el => {
+                        el.classList.remove('view-selector__item--active');
+                    });
+                    selector.classList.add('view-selector__item--active');
+                    const currentView = selector.innerText.toLowerCase().trim();
+                    activities.forEach(activity => activity.cambioVista(currentView)); 
+                });
+            });
+        });
+});
